71243c8b4ccaabd82e98f16ca5a90ec64f4951a2
Atmos_F34R <tomasaquino.edu@gmail.com>
2022-02-25 00:01:33 -0500

    testing 2

--- code/worker.c
+++ code/worker.c
@@ -1,4 +1,4 @@
-// File:	worker.c
+// File:        worker.c
 
 // List all group member's name: Yawen Xu, Tomás Aquino
 // username of iLab: yx251, tma105
@@ -21,77 +21,78 @@ worker_t* run_q;
 /* create a new thread */
 int worker_create(worker_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
 
-	// - create Thread Control Block (TCB)
-	tcb current_tcb;
-	thread = (worker_t*)malloc(sizeof(worker_t));
+        // - create Thread Control Block (TCB)
+        tcb current_tcb;
+        thread = (worker_t*)malloc(sizeof(worker_t));
 
-	// - create and initialize the context of this worker thread
-	if (getcontext(&current_tcb) < 0){
-		perror("getcontext");
-		exit(1);
-	}
+        // - create and initialize the context of this worker thread
+        if (getcontext(&current_tcb) < 0){
+                perror("getcontext");
+                exit(1);
+        }
 
-	current_tcb.wid = numWorkers;
-	current_tcb.context.uc_link = NULL;
-	current_tcb.stack = malloc(STACK_SIZE);
-	current_tcb.context.uc_stack.ss_sp = current_tcb.stack;
-	current_tcb.context.uc_stack.ss_size = STACK_SIZE;
-	current_tcb.context.uc_stack.ss_flags = 0;
-	//current_tcb.priority;
+        current_tcb.wid = numWorkers;
+        current_tcb.context.uc_link = NULL;
+        current_tcb.stack = malloc(STACK_SIZE);
+        current_tcb.context.uc_stack.ss_sp = current_tcb.stack;
+        current_tcb.context.uc_stack.ss_size = STACK_SIZE;
+        current_tcb.context.uc_stack.ss_flags = 0;
+        //current_tcb.priority;
 
-	if (current_tcb.stack == NULL){
-		perror("Failed to allocate stack");
-		exit(1);
-	}
-	makecontext(&current_tcb, (void *)function, 0);
+        if (current_tcb.stack == NULL){
+                perror("Failed to allocate stack");
+                exit(1);
+        }
+        makecontext(&current_tcb, (void *)function, 0);
 
-	// after everything is set, push this thread into run queue and 
-	// - make it ready for the execution.
-	push(run_q, tcb);
-	current_tcb.status = READY;
-	numWorkers++;
+        // after everything is set, push this thread into run queue and 
+        // - make it ready for the execution.
+        push(run_q, tcb);
+        current_tcb.status = READY;
+        numWorkers++;
 
-	return 0;
+        return 0;
 };
 
 /* give CPU possession to other user-level worker threads voluntarily */
 int worker_yield() {
-	
-	// - change worker thread's state from Running to Ready
-	current_tcb.status = READY;
-	// - save context of this thread to its thread control block
-	// - switch from thread context to scheduler context
+        
+        // - change worker thread's state from Running to Ready
+        current_tcb.status = READY;
+        current_tcb.status = READY;
+        // - save context of this thread to its thread control block
+        // - switch from thread context to scheduler context
 
-	// YOUR CODE HERE
-	
-	return 0;
+        // YOUR CODE HERE
+        
+        return 0;
 };
 
 /* terminate a thread */
 void worker_exit(void *value_ptr) {
-	// - de-allocate any dynamic memory created when starting this thread
+        // - de-allocate any dynamic memory created when starting this thread
 
-	// YOUR CODE HERE
+        // YOUR CODE HERE
 };
 
 
 /* Wait for thread termination */
 int worker_join(worker_t thread, void **value_ptr) {
-	
-	// - wait for a specific thread to terminate
-	// - de-allocate any dynamic memory created by the joining thread
+        
+        // - wait for a specific thread to terminate
+        // - de-allocate any dynamic memory created by the joining thread
   
-	// YOUR CODE HERE
-	return 0;
+        // YOUR CODE HERE
+        return 0;
 };
 
 /* initialize the mutex lock */
 int worker_mutex_init(worker_mutex_t *mutex, 
                           const pthread_mutexattr_t *mutexattr) {
-	//- initialize data structures for this mutex
+        //- initialize data structures for this mutex
 
-	// YOUR CODE HERE
-	return 0;
+        // YOUR CODE HERE
+        return 0;
 };
 
 /* aquire the mutex lock */
@@ -108,60 +109,60 @@ int worker_mutex_lock(worker_mutex_t *mutex) {
 
 /* release the mutex lock */
 int worker_mutex_unlock(worker_mutex_t *mutex) {
-	// - release mutex and make it available again. 
-	// - put threads in block list to run queue 
-	// so that they could compete for mutex later.
+        // - release mutex and make it available again. 
+        // - put threads in block list to run queue 
+        // so that they could compete for mutex later.
 
-	// YOUR CODE HERE
-	return 0;
+        // YOUR CODE HERE
+        return 0;
 };
 
 
 /* destroy the mutex */
 int worker_mutex_destroy(worker_mutex_t *mutex) {
-	// - de-allocate dynamic memory created in worker_mutex_init
+        // - de-allocate dynamic memory created in worker_mutex_init
 
-	return 0;
+        return 0;
 };
 
 /* scheduler */
 static void schedule() {
-	// - every time a timer interrupt occurs, your worker thread library 
-	// should be contexted switched from a thread context to this 
-	// schedule() function
+        // - every time a timer interrupt occurs, your worker thread library 
+        // should be contexted switched from a thread context to this 
+        // schedule() function
 
-	// - invoke scheduling algorithms according to the policy (RR or MLFQ)
+        // - invoke scheduling algorithms according to the policy (RR or MLFQ)
 
-	// if (sched == RR)
-	//		sched_rr();
-	// else if (sched == MLFQ)
-	// 		sched_mlfq();
+        // if (sched == RR)
+        //              sched_rr();
+        // else if (sched == MLFQ)
+        //              sched_mlfq();
 
-	// YOUR CODE HERE
+        // YOUR CODE HERE
 
 // - schedule policy
 #ifndef MLFQ
-	// Choose RR
+        // Choose RR
 #else 
-	// Choose MLFQ
+        // Choose MLFQ
 #endif
 
 }
 
 /* Round-robin (RR) scheduling algorithm */
 static void sched_rr() {
-	// - your own implementation of RR
-	// (feel free to modify arguments and return types)
+        // - your own implementation of RR
+        // (feel free to modify arguments and return types)
 
-	// YOUR CODE HERE
+        // YOUR CODE HERE
 }
 
 /* Preemptive MLFQ scheduling algorithm */
 static void sched_mlfq() {
-	// - your own implementation of MLFQ
-	// (feel free to modify arguments and return types)
+        // - your own implementation of MLFQ
+        // (feel free to modify arguments and return types)
 
-	// YOUR CODE HERE
+        // YOUR CODE HERE
 }
 
 // Feel free to add any other functions you need
