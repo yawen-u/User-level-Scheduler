d44363dadbc6210f3183ddf588968460e438b4af
Atmos_F34R <tomasaquino.edu@gmail.com>
2022-03-13 22:03:19 -0400

    Alternating between two function's context example * - *

--- code/sample-code/playground.c
+++ code/sample-code/playground.c
@@ -9,20 +9,32 @@
 #define STACK_SIZE SIGSTKSZ
 #define INTERVAL 500		/* number of milliseconds to go off */
 
+ucontext_t cctx, nctx, main_context;
+int flip = 0;
+struct itimerval it_val;	/* for setting itimer */
+
+
 /* function prototype */
 void DoStuff(void);
 
-void*  f1withparam(ucontext_t *nctx){
-	puts("Donald- you are threaded\n");
-	setcontext(nctx);
+void*  f1withparam(){
+	puts("Hello I am function 1\n");
 	/* setcontext sets PC <--value in nctx context*/
+	//swapcontext(&cctx, &main_context);
+	setcontext(&main_context);
 }
 
-ucontext_t cctx,nctx;
+void*  f2withparam(){
+	puts("Hello I am function 2\n");
+	/* setcontext sets PC <--value in nctx context*/
+	//swapcontext(&nctx, &main_context);
+	setcontext(&main_context);
+}
+
 
 int main(int argc, char **argv) {
 
-	struct itimerval it_val;	/* for setting itimer */
+
 
 	/* Upon SIGALRM, call DoStuff().
 	Set interval timer.  We want frequency in ms, 
@@ -44,43 +56,59 @@ int main(int argc, char **argv) {
 
 	
 	
-	if (argc != 1) {
-		printf(": USAGE Program Name and no Arguments expected\n");
-		exit(1);
-	}
 	
 	if (getcontext(&cctx) < 0){
 		perror("getcontext");
 		exit(1);
 	}
 
-	// Allocate space for stack	
-	void *stack=malloc(STACK_SIZE);
+	// Allocate space for stack1	
+	void *stack1=malloc(STACK_SIZE);
 	
-	if (stack == NULL){
-		perror("Failed to allocate stack");
+	if (stack1 == NULL){
+		perror("Failed to allocate stack1");
 		exit(1);
 	}
       
 	/* Setup context that we are going to use */
 	cctx.uc_link=NULL;
-	cctx.uc_stack.ss_sp=stack;
+	cctx.uc_stack.ss_sp=stack1;
 	cctx.uc_stack.ss_size=STACK_SIZE;
 	cctx.uc_stack.ss_flags=0;
 	
-	puts("allocate stack, attach func, with 1 parameter");
 	
 	// Make the context to start running at f1withparam()
-	makecontext(&cctx,(void *)&f1withparam,1,&nctx);
+	makecontext(&cctx,(void *)&f1withparam,0);
 	
-	puts("Successfully modified context");
+
+
+
+
+
+	if (getcontext(&nctx) < 0){
+		perror("getcontext");
+		exit(1);
+	}
+
+	// Allocate space for stack2	
+	void *stack2=malloc(STACK_SIZE);
 	
-	/* swap context will activate cctx and store location after swapcontext in nctx */
-	swapcontext(&nctx,&cctx);
+	if (stack2 == NULL){
+		perror("Failed to allocate stack2");
+		exit(1);
+	}
+      
+	/* Setup context that we are going to use */
+	nctx.uc_link=NULL;
+	nctx.uc_stack.ss_sp=stack2;
+	nctx.uc_stack.ss_size=STACK_SIZE;
+	nctx.uc_stack.ss_flags=0;
+	
+	
+	// Make the context to start running at f2withparam()
+	makecontext(&nctx,(void *)&f2withparam,0);
 
-	/* PC value in nctx will point to here */
-	puts("swap  context executed correctly \n");
-
+	
 
 
 
@@ -94,6 +122,17 @@ int main(int argc, char **argv) {
 
 void DoStuff(void) {
 
-	swapcontext(&nctx,&cctx);
+	if (flip == 0){
+
+		swapcontext(&main_context, &cctx);
+		flip = 1;	
+	}
+
+	else{
+
+		swapcontext(&main_context, &nctx);
+		flip = 0;
+	}
+	
 
 }
